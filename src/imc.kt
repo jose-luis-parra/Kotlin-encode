ddf56ec6a983a13621f723e16097d36768d14339
jose-luis-parra <joseluis.parra@gmail.com>
2024-07-22 12:25:19 +0200

    Primer commit

---
+++ src/imc.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val peso = 63
+    val altura = 172
+
+    // Añade aquí tu código
+    val imc = 10000.0 * peso / (altura*altura)
+    println("Su índice de masa corporal es $imc")
+}
