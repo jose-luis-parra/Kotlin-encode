370487e95595193a4eb6db94d7d95deee1475e80
jose-luis-parra <joseluis.parra@gmail.com>
2024-07-23 14:58:57 +0200

    Second commit

--- src/imc.kt
+++ src/imc.kt
@@ -1,8 +1,14 @@
 fun main() {
-    val peso = 63
-    val altura = 172
+    val peso = 85
+    val altura = 176
 
     // Añade aquí tu código
     val imc = 10000.0 * peso / (altura*altura)
-    println("Su índice de masa corporal es $imc")
+    println( "El valor de imc: ${"%10.1f".format(imc)}" )
+
+    if (imc < 18.5)
+        println("Tu peso está por debajo de lo normal")
+
+    if ( imc > 25 )
+        println("Tu peso está por encima de lo normal")
 }
